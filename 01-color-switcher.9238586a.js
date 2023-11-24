cc24951146dc1b3f18d7d9f67531f3238e8198ba
Belvegor <132554347+Belvegor@users.noreply.github.com>
2023-11-24 12:47:14 +0000

    Deploying to gh-pages from @ Belvegor/goit-js-hw-09@cf9d4bb604c5489460dfa9d392f8cc0200e9cea4 🚀

---
+++ 01-color-switcher.9238586a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d,o=!1;t.addEventListener("click",(()=>{o||(o=!0,t.disabled=!0,d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;a.style.backgroundColor=t}),1e3))})),e.addEventListener("click",(()=>{clearInterval(d),o=!1,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9238586a.js.map
