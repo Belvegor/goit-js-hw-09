2293fdc896235bf6d8373031c1689aef7f5ca11b
Igor <igor_socha@yahoo.com>
2023-11-28 12:48:39 +0100

    v1.4

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,41 +2,39 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-
-// wyglą interfejsu 
-function adjustElements() {
-  const fieldElements = document.querySelectorAll('.field');
-  const timerContainer = document.querySelector('.timer');
-  const datePickerInput = document.getElementById('datetime-picker');
-  const startButton = document.getElementById('startButton');
-
-  
-  //  style elementów
-  fieldElements.forEach((field) => {
-    const valueElement = field.querySelector('.value');
-    const labelElement = field.querySelector('.label');
-
-    field.style.display = 'flex';
-    field.style.flexDirection = 'column';
-    field.style.alignItems = 'center';
-
-    valueElement.style.fontSize = '50px';
-    valueElement.style.marginBottom = '5px';
-
-    labelElement.style.fontSize = '20px';
-  });
-
-  // style timera
-  timerContainer.style.display = 'flex';
-  timerContainer.style.gap = '30px';
-  timerContainer.style.marginLeft = '50px';
-
-  // style pola wyboru daty i buttona
-  datePickerInput.style.fontSize = '200%';
-  startButton.style.fontSize = '200%';
-}
-
 document.addEventListener('DOMContentLoaded', () => {
+  // Wygląd interfejsu
+  function adjustElements() {
+    const fieldElements = document.querySelectorAll('.field');
+    const timerContainer = document.querySelector('.timer');
+    const datePickerInput = document.getElementById('datetime-picker');
+    const startButton = document.getElementById('startButton');
+
+    //  style elementów
+    fieldElements.forEach((field) => {
+      const valueElement = field.querySelector('.value');
+      const labelElement = field.querySelector('.label');
+
+      field.style.display = 'flex';
+      field.style.flexDirection = 'column';
+      field.style.alignItems = 'center';
+
+      valueElement.style.fontSize = '50px';
+      valueElement.style.marginBottom = '5px';
+
+      labelElement.style.fontSize = '20px';
+    });
+
+    // style timera
+    timerContainer.style.display = 'flex';
+    timerContainer.style.gap = '30px';
+    timerContainer.style.marginLeft = '50px';
+
+    // style pola wyboru daty i buttona
+    datePickerInput.style.fontSize = '200%';
+    startButton.style.fontSize = '200%';
+  }
+
   adjustElements();
 
   // konfiguracja Flatpickr
@@ -46,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-       // sprawdza date i wyświetla komunikat ostrzegawczy
+      // sprawdza date i wyświetla komunikat ostrzegawczy
       const selectedDate = selectedDates[0];
       const currentDate = new Date();
 
@@ -63,41 +61,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let countdownInterval;
 
-  // obsługa  "Start"
-  document.getElementById("startButton").addEventListener("click", () => {
-    // pobiera pola daty i czasu Flatpickr
-    const selectedDate = flatpickr.parseDate(document.getElementById("datetime-picker").value);
-
-    // sprawdza poprawność daty i załącza odliczanie
-    if (!selectedDate) {
-      Notiflix.Notify.failure('Please select a valid date');
-      return;
-    }
-
-    countdownInterval = setInterval(() => updateCountdown(selectedDate), 1000);
-  });
-
-  // aktualizuje odliczanie do wybranej daty
   function updateCountdown(endDate) {
-    const currentDate = new Date();
-    const remainingTime = endDate - currentDate;
+    countdownInterval = setInterval(() => {
+      const currentDate = new Date();
+      const remainingTime = endDate - currentDate;
 
-    if (remainingTime <= 0) {
-      clearInterval(countdownInterval);
-      Notiflix.Notify.success('Countdown finished!');
-      return;
-    }
+      if (remainingTime <= 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("days").innerText = "00";
+        document.getElementById("hours").innerText = "00";
+        document.getElementById("minutes").innerText = "00";
+        document.getElementById("seconds").innerText = "00";
+        Notiflix.Notify.success('Countdown finished!');
+        return;
+      }
 
-  // konwertuje milisekund na dni, godziny, minuty i sekundy
-    const { days, hours, minutes, seconds } = convertMs(remainingTime);
+      const { days, hours, minutes, seconds } = convertMs(remainingTime);
 
-    // podaje pozostały czas do wyświetenia w HTML
-    document.getElementById("days").innerText = addLeadingZero(days);
-    document.getElementById("hours").innerText = addLeadingZero(hours);
-    document.getElementById("minutes").innerText = addLeadingZero(minutes);
-    document.getElementById("seconds").innerText = addLeadingZero(seconds);
+      document.getElementById("days").innerText = addLeadingZero(days);
+      document.getElementById("hours").innerText = addLeadingZero(hours);
+      document.getElementById("minutes").innerText = addLeadingZero(minutes);
+      document.getElementById("seconds").innerText = addLeadingZero(seconds);
+    }, 1000);
   }
 
+  // obsługa  "Start"
+  document.getElementById("startButton").addEventListener("click", () => {
+    const selectedDate = new Date(document.getElementById("datetime-picker").value);
+
+    // sprawdza poprawność daty i załącza odliczanie
+    if (!selectedDate || selectedDate <= new Date()) {
+      Notiflix.Notify.failure('Please select a valid future date');
+      return;
+    }
+
+// aktualizuje odliczanie do wybranej daty
+    clearInterval(countdownInterval);
+    updateCountdown(selectedDate);
+  });
+
+   // konwertuje milisekund na dni, godziny, minuty i sekundy
   function convertMs(ms) {
     const second = 1000;
     const minute = second * 60;
